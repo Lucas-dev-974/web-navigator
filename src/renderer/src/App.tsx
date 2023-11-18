4144dda00c333e1b015fe6ba700de446d174e40b
lucas <lucas.lvn97439@gmail.com>
2023-11-18 15:04:42 +0400

    use of WebContent

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,16 +1,18 @@
-import type { Component } from 'solid-js'
-import Versions from './components/Versions'
-import { HeaderApp, setSignale } from './components/header/Header'
+import type { Component } from "solid-js";
+import Versions from "./components/Versions";
+import { HeaderApp } from "./components/header/Header";
+import { WebContent } from "./views/WebContent";
 
 const App: Component = () => {
   return (
     <main class="container">
       <HeaderApp />
+      <WebContent src="https://www.google.com" />
       <footer class="footer-app">
         <Versions />
       </footer>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;

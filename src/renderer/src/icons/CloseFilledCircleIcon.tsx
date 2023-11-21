624c8db28d89d03b5911955a90327f5f20ffe274
lucas <lucas.lvn97439@gmail.com>
2023-11-21 20:26:38 +0400

    icone import CloseFilledIcon

---
+++ src/renderer/src/icons/CloseFilledCircleIcon.tsx
@@ -0,0 +1,9 @@
+import { JSXElement } from "solid-js";
+
+export function CloseFilledCircleIcon(): JSXElement {
+  return (
+    <svg width="auto" height="auto" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="6" cy="6" r="6" />
+    </svg>
+  );
+}

6d9dad337874cedcef8696f47ccb17985e7885f5
lucas <lucas.lvn97439@gmail.com>
2023-11-27 22:19:58 +0400

    add home page when no link

--- src/renderer/src/views/home/Home.tsx
+++ src/renderer/src/views/home/Home.tsx
@@ -1,9 +1,15 @@
 import { JSXElement, onMount } from "solid-js";
 
+import "./Home.css";
 export function Home(): JSXElement {
   onMount(() => {
     console.log("home mounted");
   });
 
-  return <section>Home</section>;
+  return (
+    <section class="home">
+      <p>Home</p>
+      <p>https://google.com</p>
+    </section>
+  );
 }
